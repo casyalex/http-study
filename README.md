dc4f59201cd999a2024f527a8f67b940dbb31d20
alex <superalex_1104@163.com>
2018-09-02 11:32:16 +0800

    cache-control 含义和使用

--- README.md
+++ README.md
@@ -44,3 +44,33 @@ text/plain multipart/form-data application/x-www-form-urlencoded
 2. XMLHttpRequestUpload 对象均没有注册任何事件监听器
 
 3. 请求中没有使用ReadableStream对象
+
+##### 缓存Cache-Control
+
+###### 可缓存性
+
+public 任何请求都可以对返回内容进行缓存（包括代理服务器）
+
+private 只有发起请求的服务器才可以进行缓存
+
+no-cache 任何节点都不能缓存。【会去服务器验证能不能使用缓存】
+
+###### 到期
+
+max-age = <seconds> 
+
+s-maxage = <seconds> 只有代理服务器才会生效（专为代理生效，覆盖max-age）
+
+max-stale = <seconds> max-age过期以后，还可以使用过期缓存的时间限制（发起请求带的头） 【浏览器不会主动设置这个头，一般用不到】
+
+###### 重新验证
+
+must-revalidate 如果缓存过期，必须到原服务器重新验证
+
+proxy-revalidate 缓存服务器专用的重新验证
+
+###### 其他
+
+no-store 彻底不能存储缓存 【不会去服务器验证能否使用缓存】
+
+no-transform 不允许改动返回内容（主要针对压缩）
--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((request, response) => {
-  console.log('request', request.url)
+  console.log('request come', request.url)
 
-  const html = fs.readFileSync('test.html', 'utf8')
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
-  response.end(html)
+  if (request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8')
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    response.end(html)
+  }
+
+  if (request.url === '/script.js') {
+    response.writeHead(200, {
+      'Content-Type': 'text/javascript',
+      'Cache-Control': 'max-age=200'
+    })
+    response.end('console.log("script loaded twice")')
+  }
 }).listen(8888)
 
 console.log('server listening on 8888')
